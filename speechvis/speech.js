6e7fb5ec6a30ae7dc3bdaf928d06bb99ad81ff04
ClaudiuIoanMaftei <claudiu.ioan.maftei@gmail.com>
2021-05-05 17:39:38 +0300

    Added second wave
    _kinda messy ngl_

--- speechvis/speech.js
+++ speechvis/speech.js
@@ -1,10 +1,13 @@
 let mic;
 var volhistory = [];
+var maxVerteces=360;//Infinity;
 
 var canvas_size=1200;
 
 var initial_angle=0.0;
 var sign=1;
+
+let wavey_sliders=[];
 
 function touchStarted() 
 { 
@@ -16,15 +19,14 @@ function setup() {
     
     mic = new p5.AudioIn();
     mic.start();
+    wavey_sliders[0] = createSlider(0, 0.1,0.05, 0.0001);
+    wavey_sliders[1] = createSlider(0, 0.1,0.05, 0.0001);
+    wavey_sliders[2] = createSlider(0, 0.1,0.05, 0.0001);
+    wavey_sliders[3] = createSlider(0, 4,0, 0.001);
+    
 }
 
-function draw() {
-    background(0);
-    
-    stroke(255);
-    noFill();
-    push();
-
+function draw_circleish(){
     var vol = mic.getLevel();
     if (vol!=0)
     {
@@ -82,6 +84,64 @@ function draw() {
             endShape(CLOSE);
             
         }
+}
+function draw_wavey2 () {
+    var vol = mic.getLevel();
+    if (vol!=0)
+    {
+        vol*=sign;
+        sign*=-1;
+        volhistory.push(vol);
+    }
+    const voiceAmp=800;
+    
+    const phase=millis() * 0.0005;
+    const linesAmount = 12;
+    const sinAmp=40;
+    const vertexAmount=360;
+    var dTheta=0.017;
+    
+    if (volhistory.length > maxVerteces)
+    {
+        volhistory.shift();
+        initial_angle = initial_angle + dTheta;
+    }
+    
+    for(var line = 0; line < linesAmount; line++) {
+        stroke(255, 255, 255, (line / (linesAmount - 1) * 255));
+        
+        beginShape();
+        const offset =initial_angle + line*0.2;
+        
+        for (var i = 0; i < volhistory.length; i+=1) {
+            let value = canvas_size * 0.3;
+            value += sin(i * wavey_sliders[0].value() - phase + offset) * sinAmp;
+            value += sin(i * wavey_sliders[1].value() - phase + offset) * sinAmp;
+            const offsetNoise = noise(phase*0.1 +(i / volhistory.length) * 48)*0.8;
+            value += sin(i * wavey_sliders[2].value() - phase + offset + offsetNoise) * sinAmp;
+            value += volhistory[i] * voiceAmp;
+
+            var angle=i/volhistory.length*PI*2 * 0.95;
+            var pure_x=cos(angle)*value+canvas_size/2;
+            var pure_y=sin(angle)*value+canvas_size/2;
+            var noise_value=noise(pure_x*0.001+wavey_sliders[3].value())
+            var x = pure_x + (2*noise_value-1)*800;
+            var y = value + (2*noise_value-1)*400;
+            vertex(canvas_size-i/volhistory.length*canvas_size, canvas_size/3+y);
+        }
+        endShape();
+    }
+}
+
+function draw() {
+    background(0);
+    
+    stroke(255);
+    noFill();
+    push();
+
+    draw_wavey2();
+    
     pop();
     
     
